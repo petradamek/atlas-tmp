a7fbd262675d49cb90e92106f88f24414ab4a15b
Petr Adamek <petr.adamek@bilysklep.cz>
2018-05-02 14:42:53 +0200

    wip

---
+++ web-shop/ordering-system-service/src/main/java/cz/bilysklep/trainings/webshop/ordering/service/OrderingSystemApplication.java
@@ -0,0 +1,15 @@
+package cz.bilysklep.trainings.webshop.ordering.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingSystemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrderingSystemApplication.class, args);
+        
+    }
+    
+}
